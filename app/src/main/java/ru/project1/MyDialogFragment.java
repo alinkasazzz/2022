2fbda573d476a92c33319e1ea07e864b4430a4d1
v.menshov <v.menshov@axelot.ru>
2022-07-07 21:50:14 +0300

    Добавила кликлисенеры Добавить, Удалить, Отмена.

--- app/src/main/java/ru/project1/MyDialogFragment.java
+++ app/src/main/java/ru/project1/MyDialogFragment.java
@@ -5,11 +5,13 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -21,52 +23,31 @@ import androidx.fragment.app.FragmentTransaction;
 import java.util.Objects;
 
 public class MyDialogFragment extends AppCompatDialogFragment {
-    Button buttonAdd;
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-//        try {
-//            someEventListener = (onSomeEventListener) activity;
-//        } catch (ClassCastException e) {
-//            throw new ClassCastException(activity.toString() + " must implement onSomeEventListener");
-//        }
-    } 
+
+
     final String LOG_TAG = "myLogs";
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_add, null);
 
-//        Button button = (Button) v.findViewById(R.id.button);
-//        button.setOnClickListener(new OnClickListener() {
-//            public void onClick(View v) {
-//                someEventListener.someEvent("Test text to Fragment1");
-//            }
-//        });
-
-        return v;
-    }
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(requireActivity());
         LayoutInflater inflater = requireActivity().getLayoutInflater();
-        builder.setView(inflater.inflate(R.layout.fragment_add, null))
-                .setTitle("Добавить в список");
-        Button buttonAdd = (Button) requireActivity().findViewById(R.id.button_ok);
-        Button buttonDelete = (Button) requireActivity().findViewById(R.id.button_detele);
-        buttonAdd.setOnClickListener(this::dialogAddClick);
-        buttonDelete.setOnClickListener(this::dialogDeleteClick);
+        builder.setTitle("Добавить в список");
+        View view = inflater.inflate(R.layout.fragment_add, null);
+        Button buttonOk =  view.findViewById(R.id.button_ok);
+        Button buttonCansel =  view.findViewById(R.id.button_cancel);
+
+        buttonOk.setOnClickListener(v -> Toast.makeText(getContext(),"omg",
+                Toast.LENGTH_SHORT).show());
+        buttonCansel.setOnClickListener(v -> this.dismiss());
+
+        builder.setView(view);
         return  builder.create();
     }
 
-    private void dialogAddClick(View view) {
-        TextView myTextView = (TextView) requireActivity().findViewById(R.id.myText);
-        myTextView.getText();
-    }
 
-    private void dialogDeleteClick(View view) {
 
-//        dialogInterface.cancel();
-    }
+
+
 
 }
